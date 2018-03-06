b8da8f390fcecf7f7bb16052b264a87431314d59
suresh <suresh.vallandash9@gmail.com>
2018-03-07 04:53:04 +0530

    test

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,18 +1,18 @@
 // var MongoClient = require('mongodb').MongoClient;
 const {MongoClient,ObjectID} = require('mongodb');
 
-MongoClient.connect('mongodb://localhost:27017/todoApp1',(err,db)=>{
+MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,db)=>{
 if(err){
     return console.log("Unable to connect to MongoDB");
 }console.log("Connected Succesful");
-// db.collection('Todos').insertOne({
-//     text:'Someting to do',
-//     completed: false
-// },(err,result)=>{
-// if(err){
-//     return console.log('unable to insert todo');
-// }console.log(JSON.stringify(result.ops,undefined,2));
-// });
+db.collection('Todos').insertOne({
+    text:'Someting to do',
+    completed: false
+},(err,result)=>{
+if(err){
+    return console.log('unable to insert todo');
+}console.log(JSON.stringify(result.ops,undefined,2));
+});
 // db.collection('Users').insertOne({
 //     name:'Suresh Goud',
 //     age: 25,
--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -1,16 +1,16 @@
 // var MongoClient = require('mongodb').MongoClient;
 const {MongoClient,ObjectID} = require('mongodb');
 
-MongoClient.connect('mongodb://localhost:27017/todoApp1',(err,db)=>{
+MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,db)=>{
 if(err){
     return console.log("Unable to connect to MongoDB");
 }console.log("Connected Succesful");
-// db.collection('Todos').find({completed:false}).toArray().then((docs)=>{
-// console.log('Todos');
-// console.log(JSON.stringify(docs,undefined,2));
-// },(err)=>{
-//     console.log('unable to retrieve documents');
-// })
+db.collection('Todos').find({completed:false}).toArray().then((docs)=>{
+console.log('Todos');
+console.log(JSON.stringify(docs,undefined,2));
+},(err)=>{
+    console.log('unable to retrieve documents');
+})
 
 // db.collection('Todos').find({_id:new ObjectID('5a97159a3e95052f74953b52')}).toArray().then((docs)=>{
 //     console.log('Todos');
@@ -31,11 +31,11 @@ if(err){
 // console.log('Unable to retirve users info');
 // });
 
-db.collection('Users').find({name:'Suresh'}).count().then((doc)=>{
-    console.log(doc);
-},(err)=>{
-console.log('Unable to retirve users info');
-});
+// db.collection('Users').find({text:'someting to do'}).count().then((doc)=>{
+//     console.log(doc);
+// },(err)=>{
+// console.log('Unable to retirve users info');
+// });
 
 // db.close();
 });
